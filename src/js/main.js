91c37c81467f76ce5fd0e3c0d52f08115af1050a
vladyuh <33131143+vladyuh@users.noreply.github.com>
2022-02-07 17:27:33 +0300

    fixe

--- src/js/main.js
+++ src/js/main.js
@@ -11,6 +11,109 @@ window.addEventListener("resize", function () {
     document.documentElement.style.setProperty("--vh", "".concat(vh, "px"));
 });
 
+function fileUpload() {
+    var imgData = {};
+    function sendFiles(files, elem, callback) {
+        for (var i = 0; i < files.length; i++) {
+            var file = files.item(i);
+            // проверяем что бы небыло одинаковых файлов
+            if (imgData) {
+                var count = 0
+                for (thing in imgData) {
+                    var thingObj = imgData[thing]
+                    for (thingImg in imgData[thing]) {
+                        count++;
+                        if (thingImg == file.size) {
+                            alert('Такой файл уже прикреплен, выберите другой файл ')
+                            return false;
+                        }
+                    }
+                }
+            }
+            //проверяем тип файла
+            if (file.type === 'image/jpeg' || file.type === 'image/png') {
+                var reader = new FileReader();
+                reader.readAsDataURL(file);
+                reader.onload = function (e) {
+                    var request = new XMLHttpRequest();
+                    var data = new FormData();
+                    data.append('file', file.name);
+                    data.append('data', e.target.result);
+                    request.addEventListener('loadstart', function () {
+                        var html = '' +
+                            '<div class="input-file__previewItem loading" id="file_' + e.total + '">' +
+                            '<div class="input-file__previewLoading"></div>' +
+                            '<div class="input-file__previewRemove" data-id="' + e.total + '" >&times;</div>' +
+                            '<div class="input-file__previewImage">' +
+                            '<span class="bgimage" style="background-image: url(' + e.target.result + ')"></span>' +
+                            '</div>' +
+                            '</div>';
+                        var div = document.createElement('div');
+                        div.innerHTML = html.trim();
+                        var filePreview = elem.parentNode.querySelector('.input-file__preview');
+                        filePreview.appendChild(div.firstChild);
+                    })
+
+                    request.addEventListener('readystatechange', function (dataImage) {
+                        if (request.readyState == 4) {
+                            dataImage = JSON.parse(request.responseText);
+                            callback(e, dataImage);
+                            setTimeout(function () {
+                                var file = document.querySelector('#file_' + e.total);
+                                file.classList.remove('loading');
+                                var remove = file.querySelector('.input-file__previewRemove');
+                                remove.setAttribute('data-attach-id', dataImage.attach_id);
+                            }, 1000)
+                        }
+                    })
+                    request.open("POST", 'upload.php');
+                    request.send(data);
+                }
+
+            }
+        }
+    }
+    var fileInputs = document.querySelectorAll('input[type="file"]');
+    fileInputs.forEach(element => {
+        element.addEventListener('change', function () {
+            let files = this.files;
+            let elem = element;
+            sendFiles(files, elem, function (link, dataImage) {
+                var idThing = elem.getAttribute('data-thingid');
+                if (!imgData['thing_' + idThing]) {
+                    var itemImage = imgData['thing_' + idThing] = {};
+                }
+                var itemImage = imgData['thing_' + idThing]
+                itemImage[link.total] = [dataImage.url];
+            });
+            console.log(imgData)
+        })
+    });
+    var removeAttach = document.querySelectorAll('.input-file__previewRemove');
+    document.body.addEventListener('click', function (event) {
+        if (event.target.getAttribute('data-attach-id')) {
+            elem = event.target;
+            var id = elem.getAttribute('data-id');
+            var attach_id = elem.getAttribute('data-attach-id');
+            var input = elem.closest('.input-file');
+            var idThing = input.querySelector('input[type="file"]').getAttribute('data-thingid');
+            var previewItem = elem.parentNode;
+            if (id && delete imgData['thing_' + idThing][id]) {
+                if (confirm('Удалить прикрепленное изображение?')) {
+                    previewItem.remove();
+                    var request = new XMLHttpRequest();
+                    var data = new FormData();
+                    data.append('id', attach_id);
+                    request.open("POST", 'remove.php');
+                    request.send(data);
+                }
+            }
+        };
+    });
+}
+
+var fileUploads = new fileUpload();
+
 //Mobile menu init
 function mobileMenu() {
     var toggle = document.querySelector('.header-burger .burger');
@@ -30,8 +133,8 @@ function mobileMenu() {
         body.classList.remove('mobile');
     }
 
-    this.isOpen = function(){
-        if(menu.classList.contains('opened')){
+    this.isOpen = function () {
+        if (menu.classList.contains('opened')) {
             return true;
         }
     }
@@ -41,13 +144,12 @@ var mobileMenu = new mobileMenu();
 
 document.querySelector('.header-burger .burger').addEventListener('click', function (e) {
     e.preventDefault();
-    if(mobileMenu.isOpen()){
+    if (mobileMenu.isOpen()) {
         mobileMenu.onClose();
-    }
-    else{
+    } else {
         mobileMenu.onOpen();
     }
-    
+
 });
 
 var navLinks = document.querySelectorAll('.mobileMenu-nav__ul li a');
@@ -58,8 +160,8 @@ for (var i = 0; i < navLinks.length; i++) {
 }
 
 var fieldGroup = document.querySelectorAll('.accordion__expand .toggle');
-for(var i = 0; i< fieldGroup.length; i++){
-    fieldGroup[i].addEventListener('click',function(){
+for (var i = 0; i < fieldGroup.length; i++) {
+    fieldGroup[i].addEventListener('click', function () {
         var elem = this.parentNode;
         var fields = elem.parentNode;
         this.classList.toggle('is-active');
@@ -68,11 +170,11 @@ for(var i = 0; i< fieldGroup.length; i++){
 }
 
 var fieldCollapse = document.querySelectorAll('.collapse-toggle input[type="checkbox"]');
-for(var i = 0; i< fieldCollapse.length; i++){
-    fieldCollapse[i].addEventListener('change',function(){
+for (var i = 0; i < fieldCollapse.length; i++) {
+    fieldCollapse[i].addEventListener('change', function () {
         var label = this.parentNode;
         var field = label.parentNode;
-        field.classList.toggle('is-active');        
+        field.classList.toggle('is-active');
     })
 }
 
@@ -143,38 +245,30 @@ for (var i = 0; i < submit.length; i++) {
     });
 }
 
-//ymaps
 window.addEventListener("load", function () {
-    var fileInputs = document.querySelectorAll('.custom-file-container');
     var slider = document.querySelectorAll('[data-glide-el="track"]');
-    if(fileInputs.length !== 0 ){
-        var fileUpload = document.createElement('script');
-        fileUpload.src = "/js/fileupload-with-preview.min.js";
-        fileUpload.onload = initFiles;
-        document.body.appendChild(fileUpload);
-    }
-    if(slider.length !== 0){
+    if (slider.length !== 0) {
         var glide = document.createElement('script');
         glide.src = "/js/glide.min.js";
         glide.onload = initSliders;
         document.body.appendChild(glide);
-    }       
+    }
 });
 
-function initSliders(){
+function initSliders() {
     var preview = document.querySelectorAll('.preview__slider');
     preview.forEach(element => {
-        var glide = new Glide(element,{
+        var glide = new Glide(element, {
             type: "carousel",
             perView: 6,
             gap: 10,
             breakpoints: {
-                640:{
+                640: {
                     perView: 4,
                 }
             }
-        });        
-        glide.on('move.after', function(){
+        });
+        glide.on('move.after', function () {
             var active = element.querySelector('.glide__slide--active');
             var image = element.querySelector('.glide__preview .image img');
             image.src = active.querySelector('img').getAttribute('src');
@@ -183,88 +277,61 @@ function initSliders(){
     });
 }
 
-function initFiles(){
-    var fileInput = document.querySelectorAll('.custom-file-container');
-    fileInput.forEach(element => {
-        element = new FileUploadWithPreview(element.getAttribute("data-upload-id"), {
-            showDeleteButtonOnImages: true,
-            text: {
-                chooseFile: 'Загрузить фото',
-                browse: 'Загрузить фото',
-                selectedCount: 'файла(ов) загружено',
-            },
-            images:{
-                baseImage: "",
-                backgroundImage: "",
-                successFileAltImage: "",
-                successPdfImage: "",
-                successVideoImage: "",
+function carDetails(){
+    var colorsData = {};
+    var currentPath;
+    var svg = document.querySelectorAll('.car-details__wrapper svg path[data-name]');
+    svg.forEach(element => {
+        element.addEventListener('click', function () {
+            document.querySelector('.popup-colors').style.display = "flex";            
+            document.querySelector('.popup-colors__title').textContent = element.getAttribute('data-name');
+            currentPath = this;
+
+        })
+    });
+
+    var popupClose = document.querySelector('.popup-colors__close');
+    popupClose.addEventListener('click', function(){
+        document.querySelector('.popup-colors').style.display = "none";
+    })
+
+    var popup = document.querySelectorAll('.popup-colors__item');
+    popup.forEach(element => {
+        element.addEventListener('click', function () {
+            currentPath.style.fill = this.getAttribute('data-color');
+            element.parentNode.parentNode.style.display = "none";
+
+            var idThing = currentPath.getAttribute('data-name');
+            if (!colorsData[idThing]) {
+                var itemImage = colorsData[idThing] = {};
             }
+            var itemImage = colorsData[idThing];
+            if(itemImage.current){
+                itemImage.previous = itemImage.current;
+            }
+            else{
+                itemImage.previous= "rgba(255,255,255)"; 
+            }            
+            itemImage.current = currentPath.style.fill;            
+            console.log(colorsData);
+
+            document.querySelector('.undo.btn').style.display = "flex";
+
         })
     });
 
-    var arrowRight = document.querySelectorAll('.custom-file-container__image-preview__wrapper .arrow-right');
-    arrowRight.forEach(element => {
-        element.addEventListener('click', function(){
-            var wrapper = this.parentNode;
-            var content = wrapper.querySelector('.custom-file-container__image-preview');
-            var slide = content.querySelector('.custom-file-container__image-multi-preview');
-            content.scrollBy(slide.offsetWidth + 10, 0);
-        })
-        
-    });
+    var undo  = document.querySelector('.undo.btn');
+    undo.addEventListener('click', function(){
 
-    var arrowLeft = document.querySelectorAll('.custom-file-container__image-preview__wrapper .arrow-left');
-    arrowLeft.forEach(element => {
-        element.addEventListener('click', function(){
-            var wrapper = this.parentNode;
-            var content = wrapper.querySelector('.custom-file-container__image-preview');
-            var slide = content.querySelector('.custom-file-container__image-multi-preview');
-            content.scrollBy(-(slide.offsetWidth + 10), 0);
-        })
-        
-    });
+        var current = colorsData[currentPath.getAttribute('data-name')];
+        current.current = current.previous;
+        current.previous = "rgb(255,255,255)";
+        currentPath.style.fill = current.current;
+        console.log(current);
+        console.log(colorsData)
 
-    window.addEventListener("fileUploadWithPreview:imagesAdded", function (e) {
-        var id = e.detail.uploadId;
-        var container = document.querySelector("[data-upload-id=" +id +"]");
-        container.querySelector('.custom-file-container__custom-file__custom-file-control__button').style.display= "block";
-
-        document.querySelector('.status').style.display = "flex";
-        document.querySelector('.status__message').innerText = e.detail.addedFilesCount + " изображения добавлено";
-
-        var count = e.detail.cachedFileArray;
-
-        if(count.length > 0){
-            container.querySelector('.custom-file-container__image-preview__wrapper').style.display = "block";
-        }
-
-        if(count.length > 4){
-            container.querySelector('.arrow-right').style.display ="flex"; 
-            container.querySelector('.arrow-left').style.display ="flex"; 
-        }   
-        
-    });
-
-    window.addEventListener("fileUploadWithPreview:imageDeleted", function (e) {
-        var id = e.detail.uploadId;
-        var count = e.detail.cachedFileArray;
-        var container = document.querySelector("[data-upload-id=" +id +"]");
-
-        if(count.length == 0){
-            container.querySelector('.custom-file-container__image-preview__wrapper').style.display = "none";
-        }
-
-        if(count.length <= 4){
-            container.querySelector('.arrow-right').style.display ="none"; 
-            container.querySelector('.arrow-left').style.display ="none";
-        }               
-    });
+        this.style.display = "none";
+    })
 }
 
-const statusBar = document.querySelectorAll('.status__action');
-statusBar.forEach(element => {
-    element.addEventListener('click', function(){
-        this.parentNode.style.display= "none";
-    })    
-});
+var car = new carDetails();
